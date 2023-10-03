24329e23d592173986ccd8afc6ba07cdcf166f51
Hugo Benavides <the.another.gerf@gmail.com>
2023-10-03 17:51:20 -0600

    Se agregan los iconos de desplazamiento de la barra de recomendaciones por genero, junto con sus estilos para mostrar al momento de hoover sobre la barra

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import {MdChevronLeft,MdChevronRight} from 'react-icons/md'
 import Movie from './Movie'
 
 
@@ -23,12 +24,15 @@ const Row = ({title, fetchURL}) => {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={`slider`}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft className='bg-white left-0 rounded-full opacity-50 absolute hover:opacity-100 cursor-pointer z-10 hidden group-hover:block ' size={40} />
+        
+        <div id={`slider`} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight className='bg-white right-0 rounded-full opacity-50 absolute hover:opacity-100 cursor-pointer z-10 hidden group-hover:block ' size={40} />
       </div>
     </>
   );
