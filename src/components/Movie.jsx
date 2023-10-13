2176e78bdcb4aa630b9f5fbfc37a4687fe4d183d
Hugo Benavides <the.another.gerf@gmail.com>
2023-10-12 18:17:33 -0600

    Se agrega la funcionalidad de dar like al contenido, se manda a firestore la informacion de las peliculas o series agregadas con dos variables movieID y saveShow que actualiza la db agregando un objeto con la propiedad updateDoc y arrayUnion. De esta manera se pueden insertar objetos con datos en la db

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,12 +1,36 @@
 import React,{useState} from 'react'
 import {FaHeart, FaRegHeart} from 'react-icons/fa'
+import { UserAuth } from '../context/AuthContext';
+import { db } from '../firebase';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
 
 
 const Movie = ({item}) => {
   
   const [like, setLike] = useState(false);
+  const [saved,setSaved] = useState(false);
+  const {user} = UserAuth();
 
-  console.log(like);
+  const movieID = doc(db,'users',`${user?.email}`)
+
+  const saveShow = async ()=>{
+    if(user?.email){
+      setLike(!like)
+      setSaved(true)
+      await updateDoc(movieID,{
+        savedShows: arrayUnion({
+          id: item.id,
+          title: `${item.title?item.title:item.name}`,
+          img: item.backdrop_path,
+
+        })
+      })
+    }else{
+      alert('Please log in to save a movie')
+    }
+  }
+
+  //console.log(like);
 
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280] inline-block cursor-pointer relative p-2">
@@ -21,7 +45,7 @@ const Movie = ({item}) => {
           <span>{item?.name}</span>
         </p>
       </div>
-      <p>
+      <p onClick={saveShow}>
         {like ? 
           <FaHeart className="absolute top-4 left-4 text-gray-300" />
          : 
